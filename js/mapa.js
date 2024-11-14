195592878bd4490ecbf7829ab754135aee8882e8
Gregório Ponciano <gregorioponci.91@gmail.com>
2024-11-13 23:12:40 -0300

    d

---
+++ js/mapa.js
@@ -0,0 +1,9 @@
+function showContatcs() {
+    var visualizar = document.getElementById('header-contatcs');
+
+    // Verifica o estado atual e alterna a exibição
+    if (visualizar.style.display === "block") {
+        visualizar.style.display = "none";
+    } else {
+        visualizar.style.display = "block";
+}}
