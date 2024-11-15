889895b27c3526745b4020d6b80be67725d263fc
Gregório Ponciano <gregorioponci.91@gmail.com>
2024-11-15 01:08:22 -0300

    s

--- js/show-contatcs.js
+++ js/show-contatcs.js
@@ -29,33 +29,3 @@ window.addEventListener('resize', checkScreenSizeForContacts);
 
 // Verifica o tamanho da tela quando a página for carregada
 document.addEventListener("DOMContentLoaded", () => checkScreenSizeForContacts());
-
-
-
-
-
-function showContatcs() {
-    var visualizar = document.getElementById('header-contatcs');
-
-    // Verifica o estado atual e alterna a exibição
-    if (visualizar.style.display === "block") {
-        visualizar.style.display = "none";
-    } else {
-        visualizar.style.display = "block";
-    }
-}
-
-// Função para garantir que o conteúdo seja mostrado em telas maiores
-function checkScreenSizeForContacts() {
-    var visualizar = document.getElementById('header-contatcs');
-
-    if (window.innerWidth > 1249 && visualizar.style.display === "none") {
-        visualizar.style.display = "block";  // Exibe o conteúdo se estiver oculto
-    }
-}
-
-// Adiciona o evento de redimensionamento para ajustar o conteúdo
-window.addEventListener('resize', checkScreenSizeForContacts);
-
-// Verifica o tamanho da tela quando a página for carregada
-document.addEventListener("DOMContentLoaded", () => checkScreenSizeForContacts());
